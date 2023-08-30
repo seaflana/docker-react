347a9ff17e15320fee12907a514156879a884822
seaflana <seaflana1@gmail.com>
2023-08-30 15:14:55 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to Sean's Webpage</h1>
         <p>
-          Hey there Kyle!
+          Hey there Kyle and others!!
         </p>
         <a
           href="https://www.google.com"  // Replace with the URL you want to link to
           target="_blank"               // Open link in a new tab or window
           rel="noopener noreferrer"     // Recommended for security when using target="_blank"
         >
-          Google Link
+          learn react
         </a>
         </header>
     </div>
